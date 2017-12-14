a777bd2c9b5182539140a0b0d80072d5f2799af8
Ahmed AlAskalany <ahmed.alaskalany@gmail.com>
2017-12-14 21:03:47 +0000

    Counting text and button

--- app/src/main/java/com/alaskalany/tapper/MainActivity.java
+++ app/src/main/java/com/alaskalany/tapper/MainActivity.java
@@ -2,12 +2,25 @@ package com.alaskalany.tapper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView textView = findViewById(R.id.textView);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(String.valueOf(++count));
+            }
+        });
     }
 }
